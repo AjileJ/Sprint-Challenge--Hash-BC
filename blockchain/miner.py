5f0c10f68cca4c275d06899e35c9e30e6ba735d0
AjileJ <53097022+AjileJ@users.noreply.github.com>
2020-03-13 08:55:56 -0700

    starting the mining need those coins

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -25,6 +25,10 @@ def proof_of_work(last_proof):
     print("Searching for next proof")
     proof = 0
     #  TODO: Your code here
+    last_proof = -1652267982
+    last_hash = hashlib.sha256(f"{last_proof}".encode()).hexdigest()
+    while valid_proof(last_hash, proof) is False:
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -40,7 +44,11 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+    guess = f"{proof}".encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    
+    #returns validated hash if the first 6 characters match the last 6 characters of proofs
+    return guess_hash[:6] == last_hash[-6:]
 
 
 if __name__ == '__main__':
