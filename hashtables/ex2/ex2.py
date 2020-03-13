a9579487020c31c417003aa1007b8a0f6c1e16fc
AjileJ <53097022+AjileJ@users.noreply.github.com>
2020-03-13 10:38:51 -0700

    test passed for ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -19,5 +19,16 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-
-    pass
+    #run the tickets through the hashtable passing in source and destination
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
+    # initialize routes    
+    routes = []
+    # hash none and give it an index 
+    d = hash_table_retrieve(hashtable, 'NONE')
+    # if destination is not NONE, append the destination to the routes.
+    while d is not 'NONE':
+        routes.append(d)
+        d = hash_table_retrieve(hashtable, d) 
+    # return the routes       
+    return routes    
