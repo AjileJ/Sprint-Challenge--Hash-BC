128aef77b0aa7dc10c0aa9c231e3a2998931a90a
AjileJ <53097022+AjileJ@users.noreply.github.com>
2020-03-13 09:52:30 -0700

    got the ex1 test to pass

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -12,7 +12,24 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-
+    #weights_3 = [4,6,10,15,16]
+    # answer= 21
+    # indexes (1,3)
+    # store each weights list index as its value
+    for value, key in enumerate(weights):
+        hash_table_insert(ht, key, value)
+    # check if the hash table contains an entry for limit - weight    
+    for key, value in enumerate(weights):
+        if hash_table_retrieve(ht, limit-value) is not None:
+            index = hash_table_retrieve(ht, limit - value)
+            # if higher valued index place in zeroeth spot
+            if index >= key:
+                return (index, key)
+            # else flip them 
+            else:
+                return (key, index)    
+        
+    # return none if the key does not exist in the hash table
     return None
 
 
